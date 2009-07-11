b1fec265fac26d8fe80d36e50e35adb33035e195
Jinsung Yang <jsgood.yang@samsung.com>
2009-07-11 18:09:25 +0900

    fimc dev

--- drivers/media/video/samsung/fimc2/fimc.h
+++ drivers/media/video/samsung/fimc2/fimc.h
@@ -205,6 +205,7 @@ struct fimc_scaler {
  * E X T E R N S
  *
 */
+extern struct video_device fimc_video_device[];
 extern struct s3c_platform_fimc *to_fimc_plat(struct device *dev);
 extern inline struct fimc_control *get_fimc(int id);
 extern int fimc_mapping_rot(struct fimc_control *ctrl, int degree);
