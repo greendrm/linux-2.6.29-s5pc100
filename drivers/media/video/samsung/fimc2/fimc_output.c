03d6011ceb6b9467d189ea7f11af9edce6b0dbe9
Jonghun Han <jonghun.han@samsung.com>
2009-07-11 15:24:16 +0900

    C100 FIMC

--- drivers/media/video/samsung/fimc2/fimc_output.c
+++ drivers/media/video/samsung/fimc2/fimc_output.c
@@ -24,27 +24,27 @@ int s3c_fimc_reqbufs_output(void *fh, struct v4l2_requestbuffers *b)
 {
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	if (ctrl->status != FIMC_STREAMOFF) {
-		fimc_err(ctrl->log, "FIMC is running.\n");
+		dev_err(ctrl->dev, "FIMC is running.\n");
 		return -EBUSY;
 	}
 
 	/* To do : V4L2_MEMORY_USERPTR */
 	if (b->memory != V4L2_MEMORY_MMAP) {
-		fimc_err(ctrl->log, "V4L2_MEMORY_MMAP is only supported\n");
+		dev_err(ctrl->dev, "V4L2_MEMORY_MMAP is only supported\n");
 		return -EINVAL;
 	}
 
 	if (ctrl->buf_info.requested == TRUE && b->count != 0 ) {
-		fimc_err(ctrl->log, "Buffers were already requested.\n");
+		dev_err(ctrl->dev, "Buffers were already requested.\n");
 		return -EBUSY;
 	}
 
 	/* control user input */
 	if (b->count > S3C_FIMC_OUT_BUFF_NUM) {
-		fimc_warn(ctrl->log, "The buffer count is modified by driver \
+		dev_warn(ctrl->dev, "The buffer count is modified by driver \
 				from %d to %d.\n", b->count, FIMC_OUT_BUFF_NUM);
 		b->count = FIMC_OUT_BUFF_NUM;
 	} 
@@ -52,13 +52,13 @@ int s3c_fimc_reqbufs_output(void *fh, struct v4l2_requestbuffers *b)
 	/* Initialize all buffers */
 	ret = s3c_fimc_check_out_buf(ctrl, b->count);
 	if (ret) {
-		fimc_err(ctrl->log, "Reserved memory is not sufficient.\n");
+		dev_err(ctrl->dev, "Reserved memory is not sufficient.\n");
 		return -EINVAL;
 	}
 	
 	ret = s3c_fimc_init_out_buf(ctrl);
 	if (ret) {
-		fimc_err(ctrl->log, "Cannot initialize the buffers\n");
+		dev_err(ctrl->dev, "Cannot initialize the buffers\n");
 		return -EINVAL;
 	}
 
@@ -108,10 +108,10 @@ int s3c_fimc_querybuf_output(void *fh, struct v4l2_buffer *b)
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 #if 0
 	if (ctrl->stream_status != FIMC_STREAMOFF) {
-		fimc_err(ctrl->log, "FIMC is running.\n");
+		dev_err(ctrl->dev, "FIMC is running.\n");
 		return -EBUSY;
 	}
 
@@ -142,7 +142,7 @@ int s3c_fimc_g_ctrl_output(void *fh, struct v4l2_control *c)
 		break;
 
 	default:
-		fimc_err(ctrl->log, "Invalid control id: %d\n", c->id);
+		dev_err(ctrl->dev, "Invalid control id: %d\n", c->id);
 		return -EINVAL;
 	}
 
@@ -160,7 +160,7 @@ int s3c_fimc_s_ctrl_output(void *fh, struct v4l2_control *c)
 		break;
 
 	default:
-		fimc_err(ctrl->log, "Invalid control id: %d\n", c->id);
+		dev_err(ctrl->dev, "Invalid control id: %d\n", c->id);
 		ret = -EINVAL;
 	}
 
@@ -177,10 +177,10 @@ int s3c_fimc_cropcap_output(void *fh, struct v4l2_cropcap *a)
 	unsigned int is_rot = 0;
 	unsigned int rot_degree = ctrl->s_ctrl.rot.degree;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	if (ctrl->stream_status != FIMC_STREAMOFF) {
-		fimc_err(ctrl->log, "FIMC is running.\n");
+		dev_err(ctrl->dev, "FIMC is running.\n");
 		return -EBUSY;
 	}
 
@@ -200,7 +200,7 @@ int s3c_fimc_cropcap_output(void *fh, struct v4l2_cropcap *a)
 			max_height	= ctrl->fimd.v_res;
 		}
 	} else {
-		fimc_err(ctrl->log, "V4L2_PIX_FMT_NV12, V4L2_PIX_FMT_RGB32 \
+		dev_err(ctrl->dev, "V4L2_PIX_FMT_NV12, V4L2_PIX_FMT_RGB32 \
 				and V4L2_PIX_FMT_RGB565 are only supported..\n");
 		return -EINVAL;
 	}
@@ -234,7 +234,7 @@ int s3c_fimc_s_crop_output(struct file *filp, void *fh, struct v4l2_crop *a)
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -244,7 +244,7 @@ int s3c_fimc_streamon_output(struct file *filp, void *fh, enum v4l2_buf_type i)
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -254,7 +254,7 @@ int s3c_fimc_streamoff_output(struct file *filp, void *fh, enum v4l2_buf_type i)
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -264,7 +264,7 @@ int s3c_fimc_qbuf_output(struct file *filp, void *fh, struct v4l2_buffer *b)
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -274,7 +274,7 @@ int s3c_fimc_dqbuf_output(struct file *filp, void *fh, struct v4l2_buffer *b)
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -285,7 +285,7 @@ static int s3c_fimc_g_fmt_vid_out(struct file *filp, void *fh,
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -296,7 +296,7 @@ static int s3c_fimc_s_fmt_vid_out(struct file *filp, void *fh,
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -307,7 +307,7 @@ static int s3c_fimc_try_fmt_vid_out(struct file *filp, void *fh,
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -319,7 +319,7 @@ static int s3c_fimc_g_fbuf(struct file *filp, void *fh,
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
@@ -330,7 +330,7 @@ static int s3c_fimc_s_fbuf(struct file *filp, void *fh,
 	struct fimc_control *ctrl = (struct fimc_control *) fh;
 	int ret = -1;
 
-	fimc_info(ctrl->log, "[%s] called\n", __FUNCTION__);
+	dev_info(ctrl->dev, "[%s] called\n", __FUNCTION__);
 
 	return ret;
 }
