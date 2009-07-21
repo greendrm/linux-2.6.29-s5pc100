b432ce5c0912260a329cf10a822d2f3b30068228
Jinsung Yang <jsgood.yang@samsung.com>
2009-07-21 20:05:33 +0900

    fimc dev

--- drivers/media/video/samsung/fimc2/fimc_capture.c
+++ drivers/media/video/samsung/fimc2/fimc_capture.c
@@ -161,7 +161,7 @@ int fimc_init_camera(struct fimc_control *ctrl)
 	return 0;
 }
 
-int fimc_set_scaler_info(struct fimc_control *ctrl)
+int fimc_capture_scaler_info(struct fimc_control *ctrl)
 {
 	struct fimc_scaler *sc = &ctrl->sc;
 	struct v4l2_rect *window = &ctrl->cam->window;
@@ -309,6 +309,9 @@ int fimc_s_fmt_vid_capture(struct file *file, void *fh, struct v4l2_format *f)
 
 		for (i = 0; i < FIMC_CAPBUFS; i++)
 			cap->buf[i].state = VIDEOBUF_NEEDS_INIT;
+
+		/* assign to ctrl */
+		ctrl->cap = cap;
 	}
 
 	mutex_lock(&ctrl->v4l2_lock);
@@ -351,8 +354,10 @@ int fimc_reqbufs_capture(void *fh, struct v4l2_requestbuffers *b)
 	else if (b->count < 2)
 		b->count = 2;
 
+	cap->nr_bufs = b->count;
+
 	/* alloc buffers */
-	for (i = 0; i < b->count; i++) {
+	for (i = 0; i < cap->nr_bufs; i++) {
 		cap->buf[i].base = fimc_dma_alloc(ctrl, cap->fmt.sizeimage);
 		if (!cap->buf[i].base) {
 			dev_err(ctrl->dev, "%s: no memory for " \
@@ -369,7 +374,7 @@ int fimc_reqbufs_capture(void *fh, struct v4l2_requestbuffers *b)
 	return 0;
 
 err_alloc:
-	for (i = 0; i < b->count; i++) {
+	for (i = 0; i < cap->nr_bufs; i++) {
 		if (cap->buf[i].base)
 			fimc_dma_free(ctrl, cap->fmt.sizeimage);
 
@@ -423,11 +428,39 @@ int fimc_s_crop_capture(void *fh, struct v4l2_crop *a)
 	return 0;
 }
 
+int fimc_start_capture(struct fimc_control *ctrl)
+{
+	fimc_hwset_start_scaler(ctrl);
+	fimc_hwset_enable_capture(ctrl);
+
+	return 0;
+}
+
+int fimc_stop_capture(struct fimc_control *ctrl)
+{
+	fimc_hwset_disable_capture(ctrl);
+	fimc_hwset_stop_scaler(ctrl);
+
+	return 0;
+}
+
 int fimc_streamon_capture(void *fh)
 {
 	struct fimc_control *ctrl = fh;
 	struct fimc_capinfo *cap = ctrl->cap;
-	int i;
+	int queued = 0, i;
+
+	/* checking whether all buffers are in the QUEUED state */
+	for (i = 0; i < FIMC_CAPBUFS; i++) {
+		if (cap->buf[i].state == VIDEOBUF_QUEUED)
+			queued++;
+	}
+
+	if (cap->nr_bufs != queued) {
+		dev_err(ctrl->dev, "%s: all requested buffers should " \
+			"be enqueued before streamon\n", __FUNCTION__);
+		return -EINVAL;
+	}
 
 	if (!ctrl->cam || !ctrl->cam->initialized)
 		fimc_init_camera(ctrl);
@@ -436,7 +469,7 @@ int fimc_streamon_capture(void *fh)
 	fimc_hwset_camera_offset(ctrl);
 	fimc_hwset_camera_type(ctrl);
 	fimc_hwset_camera_polarity(ctrl);
-	fimc_set_scaler_info(ctrl);
+	fimc_capture_scaler_info(ctrl);
 	fimc_hwset_prescaler(ctrl);
 	fimc_hwset_scaler(ctrl);
 	fimc_hwset_output_colorspace(ctrl, cap->fmt.pixelformat);
@@ -451,32 +484,108 @@ int fimc_streamon_capture(void *fh)
 	fimc_hwset_output_area(ctrl, cap->fmt.width, cap->fmt.height);
 	fimc_hwset_org_output_size(ctrl, cap->fmt.width, cap->fmt.height);
 
-	for (i = 0; i < FIMC_CAPBUFS; i++)
+	/* 
+	 * we can make sure that all requested buffers are in QUEUED state and,
+	 * all QUEUED buffers are placed from 0 index sequentially
+	*/
+	for (i = 0; i < cap->nr_bufs; i++)
 		fimc_hwset_output_address(ctrl, i, cap->buf[i].base, &cap->fmt);
 
-	fimc_hwset_start_scaler(ctrl);
-	fimc_hwset_enable_capture(ctrl);
+	fimc_start_capture(ctrl);
 	
 	return 0;
 }
 
 int fimc_streamoff_capture(void *fh)
 {
+	struct fimc_control *ctrl = fh;
+
+	fimc_stop_capture(ctrl);
+
+	return 0;
+}
+
+int fimc_count_capbuf(struct fimc_capinfo *cap, enum videobuf_state state)
+{
+	int cnt = 0, i;
+
+	for (i = 0; i < FIMC_CAPBUFS; i++) {
+		if (cap->buf[i].state == state)
+			cnt++;
+	}
+
+	return cnt;
+}
+
+int fimc_find_capbuf(struct fimc_capinfo *cap, 
+			enum videobuf_state state, int index)
+{
+	int cnt, i;
+
+	cnt = 0;
+	for (i = 0; i < FIMC_CAPBUFS; i++) {
+		if (cap->buf[i].state == state)
+			cnt++;
+
+		if (cnt - 1 == index)
+			return i;
+	}
+
+	return -ENOENT;
+}
+
+int fimc_rearrange_capbufs(struct fimc_control *ctrl)
+{
+	struct fimc_capinfo *cap = ctrl->cap;
+	int qbufs, actual, i, j, bi, ref;
+
+	qbufs = fimc_count_capbuf(cap, VIDEOBUF_QUEUED);
+	actual = get_actual_bufnum(qbufs);
+
+	for (i = 0; i < actual; i++) {
+		bi = fimc_find_capbuf(cap, VIDEOBUF_QUEUED, i);
+		fimc_hwset_output_address(ctrl, i, \
+			cap->buf[bi].base, &cap->fmt);
+	}
+
+	for (j = i; j < FIMC_PHYBUFS; j++) {
+		ref = j - actual;
+		fimc_hwset_output_address(ctrl, j, \
+			cap->buf[ref].base, &cap->fmt);
+	}
+
 	return 0;
 }
 
 int fimc_qbuf_capture(void *fh, struct v4l2_buffer *b)
 {
 	struct fimc_control *ctrl = fh;
+	struct fimc_capinfo *cap = ctrl->cap;
 
 	if (b->memory != V4L2_MEMORY_MMAP) {
 		dev_err(ctrl->dev, "%s: invalid memory type\n", __FUNCTION__);
 		return -EINVAL;
 	}
 
+	if (cap->buf[b->index].state == VIDEOBUF_QUEUED) {
+		dev_err(ctrl->dev, "%s: already in QUEUE state\n", \
+			__FUNCTION__);
+		return -EINVAL;
+	}
+
 	mutex_lock(&ctrl->v4l2_lock);
 
-	ctrl->cap->buf[b->index].state = VIDEOBUF_QUEUED;
+	if (fimc_count_capbuf(cap, VIDEOBUF_QUEUED) < cap->nr_bufs) {
+		fimc_stop_capture(ctrl);
+		ctrl->cap->buf[b->index].state = VIDEOBUF_QUEUED;
+		fimc_rearrange_capbufs(ctrl);
+		fimc_start_capture(ctrl);
+	} else {
+		dev_err(ctrl->dev, "%s: no space for this qbuf\n", \
+			__FUNCTION__);
+		mutex_unlock(&ctrl->v4l2_lock);
+		return -ENOENT;
+	}
 
 	mutex_unlock(&ctrl->v4l2_lock);
 
@@ -486,6 +595,8 @@ int fimc_qbuf_capture(void *fh, struct v4l2_buffer *b)
 int fimc_dqbuf_capture(void *fh, struct v4l2_buffer *b)
 {
 	struct fimc_control *ctrl = fh;
+	struct fimc_capinfo *cap = ctrl->cap;
+	int i;
 
 	if (b->memory != V4L2_MEMORY_MMAP) {
 		dev_err(ctrl->dev, "%s: invalid memory type\n", __FUNCTION__);
@@ -494,7 +605,18 @@ int fimc_dqbuf_capture(void *fh, struct v4l2_buffer *b)
 
 	mutex_lock(&ctrl->v4l2_lock);
 
-	/* TODO */
+	if (fimc_count_capbuf(cap, VIDEOBUF_DONE) > 1) {
+		fimc_stop_capture(ctrl);
+		i = fimc_hwget_frame_count(ctrl);
+		ctrl->cap->buf[i].state = VIDEOBUF_IDLE;
+		fimc_rearrange_capbufs(ctrl);
+		fimc_start_capture(ctrl);		
+	} else {
+		dev_err(ctrl->dev, "%s: no buffer for this dqbuf\n", \
+			__FUNCTION__);
+		mutex_unlock(&ctrl->v4l2_lock);
+		return -ENOENT;
+	}
 
 	mutex_unlock(&ctrl->v4l2_lock);
 	
