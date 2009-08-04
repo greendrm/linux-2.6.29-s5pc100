4b3eba89da3fd47ebe6ae4a7034433824193d096
mogy <ym.song@samsung.com>
2009-08-04 18:35:07 +0900

    Add IPC for FIMC

---
+++ arch/arm/plat-s5pc11x/include/plat/ipc.h
@@ -0,0 +1,27 @@
+/* linux/arch/arm/plat-s5pc11x/include/plat/ipc.h
+ *
+ * Platform header file for IPC driver
+ *
+ * Youngmok Song, Copyright (c) 2009 Samsung Electronics
+ * 	http://www.samsungsemi.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _IPC_H
+#define _IPC_H
+
+struct platform_device;
+
+struct s3c_platform_ipc {
+	const char	clk_name[16];
+
+	void		(*cfg_gpio)(struct platform_device *dev);
+};
+
+extern void s3c_ipc_set_platdata(struct s3c_platform_ipc *csis);
+extern void s3c_ipc_cfg_gpio(struct platform_device *dev);
+
+#endif /* _IPC_H */
