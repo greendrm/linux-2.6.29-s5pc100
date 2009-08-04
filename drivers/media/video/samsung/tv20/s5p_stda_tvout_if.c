8dcba5082774efb0a87fc4839e614bb200b8bcdb
SangPil Moon <sangpil.moon@samsung.com>
2009-08-05 00:29:15 +0900

    [TV20] modified for compatibility between c100 & c110

--- drivers/media/video/samsung/tv20/s5p_stda_tvout_if.c
+++ drivers/media/video/samsung/tv20/s5p_stda_tvout_if.c
@@ -281,11 +281,13 @@ bool _s5p_tv_if_init_vm_reg(void)
 
 		case TVOUT_720P_50:
 
+#ifdef CONFIG_CPU_S5PC110
 		case TVOUT_1080P_60:
 
 		case TVOUT_1080P_50:
+#endif				
 			__s5p_vm_init_csc_coef_default(VMIXER_CSC_RGB_TO_YUV709_FR);
-			break;
+		break;
 		}
 
 		break;
@@ -705,7 +707,7 @@ bool _s5p_tv_if_init_avi_frame(tvout_output_if* tvout_if)
 		st->avi_byte[3] = AVI_VIC_4;
 		break;
 
-// C110
+#ifdef CONFIG_CPU_S5PC110
 	case TVOUT_1080P_50:
 		st->avi_byte[1] |= AVI_PAR_16_9;
 		st->avi_byte[3] = AVI_VIC_31;
@@ -715,7 +717,7 @@ bool _s5p_tv_if_init_avi_frame(tvout_output_if* tvout_if)
 		st->avi_byte[1] |= AVI_PAR_16_9;
 		st->avi_byte[3] = AVI_VIC_16;
 		break;		
-
+#endif
 	default:
 		TVOUTIFPRINTK("invalid disp_mode parameter(%d)\n\r", 
 			tvout_if->out_mode);
@@ -870,12 +872,13 @@ bool _s5p_tv_if_start(void)
 		case TVOUT_720P_60:
 			__s5p_tv_clk_init_hpll(0xffff, 132, 6, 3);
 			break;
-//C110
+#ifdef CONFIG_CPU_S5PC110
 		case TVOUT_1080P_50:
 
 		case TVOUT_1080P_60:
 			__s5p_tv_clk_init_hpll(0xffff, 132, 6, 2);
 			break;
+#endif			
 
 		default:
 			_s5p_tv_if_stop();
@@ -1053,12 +1056,12 @@ bool _s5p_tv_if_set_disp(void)
 
 	case TVOUT_720P_60:
 
-//C110:
-	
-	
+#ifdef CONFIG_CPU_S5PC110
+
 	case TVOUT_1080P_50:
 
 	case TVOUT_1080P_60:
+#endif		
 		if (!_s5p_tv_if_init_avi_frame(&st->tvout_param)) {
 			st->tvout_param_available = false;
 			return false;
