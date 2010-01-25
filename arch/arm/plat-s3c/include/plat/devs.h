4b035a817c2c378a1c72d2e6621dcc5edb0134eb
Jassi Brar <jassi.brar@samsung.com>
2010-01-25 17:23:40 +0900

    ARM: S3C64XX: Add AC97 platform resources
    This patch defines the platform device and the resources: IRQ, DMA and MEM,
    needed by the AC97 controller driver.
    
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>

--- arch/arm/plat-s3c/include/plat/devs.h
+++ arch/arm/plat-s3c/include/plat/devs.h
@@ -30,6 +30,9 @@ extern struct platform_device s3c_device_timer[];
 extern struct platform_device s3c_device_usb;
 extern struct platform_device s3c_device_usb_ohci;
 extern struct platform_device s3c_device_usb_ehci;
+
+extern struct platform_device s3c64xx_device_ac97;
+
 extern struct platform_device s3c_device_lcd;
 extern struct platform_device s3c_device_g2d;
 extern struct platform_device s3c_device_g3d;
