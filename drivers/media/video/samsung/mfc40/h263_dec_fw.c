6fe399cd35a64b54741844958d24dea417e8ac9a
Sungjun Bae <june.bae@samsung.com>
2010-04-12 16:28:29 +0900

    S5PC100: MFC: GPL license text is removed.
    GPL license text is removed on firmware codes.
    
    Signed-off-by: Sungjun Bae <june.bae@samsung.com>

--- drivers/media/video/samsung/mfc40/h263_dec_fw.c
+++ drivers/media/video/samsung/mfc40/h263_dec_fw.c
@@ -6,9 +6,6 @@
  * PyoungJae Jung, Jiun Yu, Copyright (c) 2009 Samsung Electronics
  * http://www.samsungsemi.com/
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 const unsigned char h263_dec_mc_fw[9833] = {
