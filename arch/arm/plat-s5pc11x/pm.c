dd6b759afbdd92061ddef1960e885292523ee7fa
Jaecheol Lee <jc.lee@samsung.com>
2010-04-21 16:06:34 +0900

    S5PC110: PM: Modified header description of GPL
    GPL description is modified.
    
    Signed-off-by: Jaecheol Lee <jc.lee@samsung.com>

--- arch/arm/plat-s5pc11x/pm.c
+++ arch/arm/plat-s5pc11x/pm.c
@@ -1,27 +1,15 @@
 /* linux/arch/arm/plat-s5pc11x/pm.c
  *
- * Copyright (c) 2004,2009 Simtec Electronics
- *	boyko.lee <boyko.lee@samsung.com>
+ * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ * 		http://www.samsung.com
  *
  * S5PC11X Power Manager (Suspend-To-RAM) support
  *
+ * Based on plat-s3c24xx/pm.c
+ *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Parts based on arch/arm/mach-pxa/pm.c
- *
- * Thanks to Dimitry Andric for debugging
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
 */
 
 #include <linux/init.h>
