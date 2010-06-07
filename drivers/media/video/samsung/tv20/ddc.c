fc68d4fb3d85480efebac0ed1c22c47abf096237
Sangpil Moon <sangpil.moon@samsung.com>
2010-06-07 13:23:01 +0900

    S5PC110: TVOUT: Added copyright of ddc client driver
    Added copyright of ddc client driver
    
    Signed-off-by: Sangpil Moon <sangpil.moon@samsung.com>

--- drivers/media/video/samsung/tv20/ddc.c
+++ drivers/media/video/samsung/tv20/ddc.c
@@ -1,3 +1,15 @@
+/* linux/drivers/media/video/samsung/tv20/ddc.c
+ *
+ * ddc i2c client driver file for Samsung TVOut driver
+ *
+ * Copyright (c) 2009 Samsung Electronics
+ * 	http://www.samsungsemi.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/i2c.h>
--- drivers/media/video/samsung/tv20/ddc.h
+++ drivers/media/video/samsung/tv20/ddc.h
@@ -1,3 +1,15 @@
+/* linux/drivers/media/video/samsung/tv20/ddc.h
+ *
+ * ddc i2c client driver header file for Samsung TVOut driver
+ *
+ * Copyright (c) 2009 Samsung Electronics
+ * 	http://www.samsungsemi.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
 /* 
  * i2c ddc port 
  */
