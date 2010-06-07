be11554b4338f4b8d6c11ffefef8b458277eb7cc
Hyunwoong Kim <khw0178.kim@samsung.com>
2010-06-07 11:47:29 +0900

    S5PC110: FIMD: Modified copyright of tl2796 LDI driver
    Modified copyright of tl2796 LDI driver

--- drivers/video/samsung/s3cfb_tl2796.c
+++ drivers/video/samsung/s3cfb_tl2796.c
@@ -1,24 +1,14 @@
-/*
- * TL2796 LCD Panel Driver for the Samsung Universal board
+/* linux/drivers/video/samsung/s3cfb_tl2796.c
  *
- * Author: InKi Dae  <inki.dae@samsung.com>
+ * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ * 		http://www.samsung.com
  *
- * Derived from drivers/video/omap/lcd-apollon.c
+ * TL2796 LCD Panel control 
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
 
 #include <linux/wait.h>
 #include <linux/fb.h>
